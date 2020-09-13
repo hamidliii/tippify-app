51a4ee0035eb19f589559cc2f5e74d1c0c367cd7
Nina Hamidli <nigar.hamidli1999@gmail.com>
2020-09-14 00:53:47 +0200

    Initial Commit

---
+++ tippify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippify
+//
+//  Created by Nina on 9/14/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
